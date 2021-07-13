3da51a70d682e7700bd0da1e761a5e2c3c08b590
sekiguchi0215 <dsphere5296@gmail.com>
2021-07-13 22:16:33 +0900

    コマンドを利用してメイラーとビューファイルを作成

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,24 @@
+class ContactMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.contact_mailer.user_email.subject
+  #
+  def user_email
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.contact_mailer.admin_email.subject
+  #
+  def admin_email
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
