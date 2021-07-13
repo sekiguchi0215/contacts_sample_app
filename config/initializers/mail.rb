7d917da22f8141fffc961b03e54223f6c1c61534
sekiguchi0215 <dsphere5296@gmail.com>
2021-07-13 23:05:38 +0900

    各所の修正

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,14 +1,15 @@
 if Rails.env.production?
-  host = still-coast-65256.herokuapp.com
+  host = "still-coast-65256.herokuapp.com"
 
   ActionMailer::Base.raise_delivery_errors = true
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     port: 587,
-    address: "smtp.gmail.com",
+    address: 'smtp.gmail.com',
     user_name: Rails.application.credentials.gmail[:address],
+    password: Rails.application.credentials.gmail[:password],
     domain: host,
-    authentication: "plain"
+    authentication: 'plain'
   }
 end
