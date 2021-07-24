cf46e9728acdc25d4efc0040d45f420062a909d0
sekiguchi0215 <dsphere5296@gmail.com>
2021-07-25 01:15:18 +0900

    バリデーションを記述

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,8 @@
 class Contact < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name, lenght: { maximum: 48}
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, lenght: { maximum: 256 }
+  validates :contact, presence: true, lenght: { maximum: 2000 }
+  validates :remote_ip, presence: true
 end
