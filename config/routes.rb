e284ca2e03941e023bb2917cfb527b70c724c6b1
sekiguchi0215 <dsphere5296@gmail.com>
2021-07-25 01:55:16 +0900

    コントローラに処理を記述

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,4 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   root "homes#index"
   resources :contacts, only: %i[index create]
-  
 end
